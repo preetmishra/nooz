aefc9e208a15a6b90317f79ec028a19d018fb1bb
Preet Mishra <ipreetmishra@gmail.com>
2020-03-12 21:56:03 +0530

    widgets: Add WIDGET_SIZE for AboutTextWidget.
    This will help in determining the size for the right column view.

--- nooz/widgets.py
+++ nooz/widgets.py
@@ -5,6 +5,9 @@ class AboutTextWidget(urwid.WidgetWrap):
     '''
     Information about the app.
     '''
+
+    WIDGET_SIZE = 2
+
     def __init__(self) -> None:
         desc = 'nooz: Trending headlines right in your terminal.'
         self._w = urwid.Text(desc)
