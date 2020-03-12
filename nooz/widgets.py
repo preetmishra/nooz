4def8f04c70856833754b535d1950a3e7cff0f94
Preet Mishra <ipreetmishra@gmail.com>
2020-03-12 23:37:33 +0530

    widgets: Add widgets for searching news.
    Added KeywordSearchWidget, CategorySearchWidget and SearchWidget.
    SearchWidget is a base class from which KeywordSearchWidget and
    CategorySearchWidget inherit.

--- nooz/widgets.py
+++ nooz/widgets.py
@@ -1,4 +1,7 @@
+from typing import Any
 import urwid
+
+from helper import CATEGORIES, COUNTRIES
 
 
 class AboutTextWidget(urwid.WidgetWrap):
@@ -11,3 +14,75 @@ class AboutTextWidget(urwid.WidgetWrap):
     def __init__(self) -> None:
         desc = 'nooz: Trending headlines right in your terminal.'
         self._w = urwid.Text(desc)
+
+
+# FIXME: Add missing type annotations.
+class SearchWidget(urwid.WidgetWrap):
+    '''
+    Base class for search widgets.
+    '''
+
+    def __init__(self, controller: Any, search_text: str, body,
+                 widget_size: int) -> None:
+        self.controller = controller
+        self.search_text = search_text
+        self.search = urwid.Edit(self.search_text)
+        self.body = body
+        frame = urwid.Frame(body=self.body, header=self.search,
+                            focus_part='header')
+        self._w = urwid.BoxAdapter(frame, widget_size)
+
+    def keypress(self, size, key: str):
+        if key == 'enter' or key == 'esc':
+            self.search.set_edit_text('')
+            self.controller.set_main_focus('right')
+        return super().keypress(size, key)
+
+
+# FIXME: Add missing type annotations.
+class KeywordSearchWidget(SearchWidget):
+    '''
+    This widget is a part of the left column view.
+    It provides an interface for searching news using any keyword.
+    '''
+
+    WIDGET_SIZE = 4
+
+    def __init__(self, controller: Any) -> None:
+        body = urwid.Filler(
+            urwid.Text('Examples: apple, hollywood, trump, etc.'),
+            valign='bottom'
+        )
+        super().__init__(controller, 'Search by keyword: ', body,
+                         self.WIDGET_SIZE)
+
+    def keypress(self, size, key: str):
+        if key == 'enter':
+            keyword = self.search.get_edit_text().lower()
+            self.controller.update_right_column_view(keyword=keyword)
+        return super().keypress(size, key)
+
+
+# FIXME: Add missing type annotations.
+class CategorySearchWidget(SearchWidget):
+    '''
+    This widget is a part of the left column view.
+    It provides an interface for searching news by a category.
+    '''
+
+    WIDGET_SIZE = len(CATEGORIES) + 2
+
+    def __init__(self, controller: Any) -> None:
+        categories = urwid.Pile([
+            urwid.Text('* ' + category[0].upper() + category[1:])
+            for category in CATEGORIES
+        ])
+        body = urwid.Filler(categories, valign='bottom')
+        super().__init__(controller, 'Search by category: ', body,
+                         self.WIDGET_SIZE)
+
+    def keypress(self, size, key: str):
+        if key == 'enter':
+            category = self.search.get_edit_text().lower()
+            self.controller.update_right_column_view(category=category)
+        return super().keypress(size, key)
